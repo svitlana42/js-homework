382946038fe57a2d3f9f41788b46248da601902b
svitlana42 <silcenko959@gmail.com>
2026-06-06 00:35:15 +0300

    hw

---
+++ 18/form.js
@@ -0,0 +1,98 @@
+window.onload = function () {
+    // 1
+    let myColor = document.querySelectorAll('input[name="color"]');
+    myColor.forEach(function (item) {
+        item.addEventListener("change", function () {
+            document.body.style.backgroundColor = this.value;
+        });
+    });
+
+    // 2
+    let reading = document.querySelector("#reading");
+    let sport = document.querySelector("#sport");
+    let music = document.querySelector("#music");
+    let result = document.querySelector(".result");
+
+    reading.onchange = hobby;
+    sport.onchange = hobby;
+    music.onchange = hobby;
+
+    function hobby() {
+        let hobbies = "";
+        if (reading.checked) {
+            hobbies += reading.value + ", ";
+        }
+        if (sport.checked) {
+            hobbies += sport.value + ", ";
+        }
+        if (music.checked) {
+            hobbies += music.value + ", ";
+        }
+        hobbies = hobbies.slice(0, -2);
+
+        result.textContent = "Ваші хобі: " + hobbies;
+    }
+
+    //3
+    let country = document.querySelector("#country");
+    let info = document.querySelector(".info");
+
+    country.onchange = function () {
+        if (country.value === "ukraine") {
+            info.textContent = "Столиця: Киів. Населення: 41млн.";
+        }
+        if (country.value === "germany") {
+            info.textContent = "Столиця: Берлін. Населення: 83млн.";
+        }
+        if (country.value === "england") {
+            info.textContent = "Столиця: Лондон. Населення: 58млн.";
+        }
+    };
+
+    // 4
+    let rating = document.querySelectorAll('input[name="rating"]');
+    let message = document.querySelector(".rate");
+
+    for (let i = 0; i < rating.length; i++) {
+        rating[i].onchange = function () {
+            message.textContent = "Дякуємо за вашу оцінку: " + this.value;
+        };
+    }
+
+    // 5
+    let pizza = document.querySelector("#pizza");
+    let sizes = document.querySelectorAll('input[name="size"]');
+    let toppings = document.querySelectorAll(".topping");
+    let price = document.querySelector(".price");
+
+    pizza.onchange = pizzaPrice;
+
+    for (let i = 0; i < sizes.length; i++) {
+        sizes[i].onchange = pizzaPrice;
+    }
+    for (let i = 0; i < toppings.length; i++) {
+        toppings[i].onchange = pizzaPrice;
+    }
+
+    function pizzaPrice() {
+        if (pizza.value === "") {
+            price.textContent = "Оберіть піцу";
+            return;
+        }
+
+        let sum = Number(pizza.value);
+
+        for (let i = 0; i < sizes.length; i++) {
+            if (sizes[i].checked) {
+                sum += Number(sizes[i].value);
+            }
+        }
+        for (let i = 0; i < toppings.length; i++) {
+            if (toppings[i].checked) {
+                sum += 25;
+            }
+        }
+        price.textContent = "Ціна: " + sum + " грн.";
+    }
+}
+
