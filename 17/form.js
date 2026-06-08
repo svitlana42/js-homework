0198387911c21db68b206eea3982d948b5f91d7a
svitlana42 <silcenko959@gmail.com>
2026-06-08 15:35:36 +0300

    hw

--- 17/form.js
+++ 17/form.js
@@ -1,32 +1,14 @@
 window.onload = function () {
     //1 
-    let amount = document.createElement("input");
-    amount.placeholder = "Введіть суму";
-    amount.style.marginBottom = "20px";
-    document.body.append(amount);
-
-    let course = document.createElement("input");
-    course.placeholder = "Введіть курс обміну";
-    document.body.append(course);
-
-    let converter = document.createElement("button");
-    converter.textContent = "Конвертувати";
-    document.body.append(converter);
-
-    let clear = document.createElement("button");
-    clear.textContent = "Очистити";
-    clear.style.marginLeft = "15px";
-    document.body.append(clear);
-
-    let result = document.createElement("p");
-    result.style.marginBottom = "20px";
-    document.body.append(result);
+    let amount = document.querySelector("#amount");
+    let course = document.querySelector("#course");
+    let converter = document.querySelector("#converter");
+    let clear = document.querySelector("#clear");
+    let result = document.querySelector("#result");
 
     converter.onclick = function () {
-        let amountInput = amount.value;
-        let courseInput = course.value;
-        let myResult = amountInput * courseInput;
-        result.textContent = "Результат:" + myResult;
+        let myResult = amount.value * course.value;
+        result.textContent = "Результат: " + myResult;
     };
 
     clear.onclick = function () {
@@ -36,16 +18,10 @@ window.onload = function () {
     };
 
     // 2
-    let task = document.createElement("input");
-    task.placeholder = "Задайте завдання";
-    document.body.append(task);
+    let task = document.querySelector("#task");
+    let btn = document.querySelector("#btn");
+    let list = document.querySelector("#list");
 
-    let btn = document.createElement("button");
-    btn.textContent = "Додати задачу";
-    document.body.append(btn);
-
-    let list = document.createElement("ul");
-    document.body.append(list);
 
     btn.onclick = function () {
         if (task.value === "") {
@@ -54,11 +30,10 @@ window.onload = function () {
         }
         let item = document.createElement("li");
         item.textContent = task.value;
-        item.style.marginTop = "10px";
 
         let btn1 = document.createElement("button");
         btn1.textContent = "Видалити";
-        btn1.style.marginLeft = "15px";
+        btn1.style.margin = "10px";
 
         item.append(btn1);
         list.append(item);
