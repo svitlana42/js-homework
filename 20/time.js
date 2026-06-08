52a023da83f9c44827c5a29a53065523302d51cb
svitlana42 <silcenko959@gmail.com>
2026-06-08 21:52:50 +0300

    hw

---
+++ 20/time.js
@@ -0,0 +1,37 @@
+window.onload = function () {
+    let time = document.querySelector(".time");
+    let startBtn = document.querySelector(".start");
+    let resetBtn = document.querySelector(".reset");
+
+    let seconds = 1500;
+    let timerId;
+
+    function myTime() {
+        let min = Math.floor(seconds / 60);
+        let sec = seconds % 60;
+
+        if (sec < 10) {
+            sec = "0" + sec;
+        }
+
+        time.textContent = min + ":" + sec;
+    }
+
+    startBtn.onclick = function () {
+        clearInterval(timerId);
+        timerId = setInterval(function () {
+            seconds--;
+            myTime();
+
+            if (seconds <= 0) {
+                clearInterval(timerId);
+                alert("Час вийшов");
+            }
+        }, 1000);
+    };
+    resetBtn.onclick = function () {
+        clearInterval(timerId);
+        seconds = 1500;
+        myTime();
+    };
+};
