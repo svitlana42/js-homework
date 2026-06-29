fbba9ceab8fc912920b9b64383ac740ce0b7f899
svitlana42 <silcenko959@gmail.com>
2026-06-29 20:17:35 +0300

    hw 28

---
+++ 28/vue-app/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+const isChecked = ref(false)
+</script>
+
+<template>
+  <HelloWorld />
+  
+  <div style="margin: 10px; text-align: center;">
+    <label style="cursor: pointer; font-size: 18px; color: yellow;">
+      <input type="checkbox" v-model="isChecked" /> 
+      Показати текст
+    </label>
+
+    <p v-if="isChecked" style="color: green; font-size: 20px; margin-top: 15px; font-weight: 700;">
+      Секретна область розблокована
+    </p>
+  </div>
+</template>
